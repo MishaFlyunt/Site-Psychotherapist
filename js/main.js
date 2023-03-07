8fbf4522dc87cc2a12ecb90ac95773b545facc22
Misha Flyunt <98420437+MishaFlyunt@users.noreply.github.com>
2023-03-07 21:32:40 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -13,3 +13,21 @@ for (let i = 0; i < btn.length; i++) {
     }
   });
 }
+
+// ------------Modal---------
+
+(() => {
+	const refs = {
+		openModalBtn: document.querySelector("[data-modal-open]"),
+		closeModalBtn: document.querySelector("[data-modal-close]"),
+		modal: document.querySelector("[data-modal]"),
+	};
+
+	refs.openModalBtn.addEventListener("click", toggleModal);
+	refs.closeModalBtn.addEventListener("click", toggleModal);
+
+	function toggleModal() {
+		refs.modal.classList.toggle("is-hidden");
+	}
+})();
+
