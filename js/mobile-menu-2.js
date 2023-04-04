a3ddb6bfe79eeb7c1f84c6e17a68fd1d3a605196
Misha Flyunt <98420437+MishaFlyunt@users.noreply.github.com>
2023-04-04 10:58:21 +0300

    commit

--- js/mobile-menu-2.js
+++ js/mobile-menu-2.js
@@ -27,19 +27,3 @@ if (menu && menuBtn) {
 	});
 }
 
-/*===========================================*/
-
-const anchors = document.querySelectorAll(' a[href*="#"] ');
-
-anchors.forEach(anchor => {
-	anchor.addEventListener("click", event => {
-		event.preventDefault();
-
-		const blockID = anchor.getAttribute("href").substring(1);
-
-		document.getElementById(blockID).scrollIntoView({
-			behavior: "smooth",
-			block: "start",
-		});
-	});
-});
